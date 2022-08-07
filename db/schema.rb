186da01b5711032fd64d056cc3d74eef6e7af8b3
johnsey <johnsey.reynolds@gmail.com>
2022-08-07 14:23:08 -0400

    commit tree cafaed55902382a53f8073c7fc8a6a1e2d6c6239

--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 1) do
-
+ActiveRecord::Schema.define(version: 3) do
+    t.string "name"
+    t.integer "grade"
+    t.datetime "birthdate"
+  end 
 end
